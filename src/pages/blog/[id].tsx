04283e6407cbdd0836f648b44d3ce9f66d8f00e0
tktk7l9 <saitotakuya0719@gmail.com>
2021-01-07 13:16:19 +0900

    ブログのOGPを動的に

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -12,13 +12,14 @@ export default function BlogId({
 }: {
   blog: Article | Draft;
   preview: boolean;
-}) {
+  }) {
+    const utf8title = unescape(encodeURIComponent(blog.title));
   return (
     <>
       <Meta
         title={blog.title}
         description=""
-        image="https://images.microcms-assets.io/protected/ap-northeast-1:12587faf-e507-4a34-8fbf-d773ad9ed641/service/tktk7l9/media/asakusashi_dance.jpg"
+        image={`https://og-image-1.vercel.app/${utf8title}.png`}
       />
       <Header />
       {preview && <a href="/api/clear-preview">プレビューモードを解除</a>}

53fe17e568adaa8e9f863f80eb27b9dc81273d89
tktk7l9 <saitotakuya0719@gmail.com>
2021-01-14 21:23:10 +0900

    アクセシビリティ

--- src/components/cards/ProfileCard.tsx
+++ src/components/cards/ProfileCard.tsx
@@ -13,14 +13,14 @@ export const ProfileCard: FC<Props> = (props) => {
         <img
           src="/profile.jpg"
           alt="asakusa-shi"
-          className="h-full w-full object-contain"
+          className="h-40 w-40 object-contain"
         />
       </div>
 
       <h2 className="mt-4 font-bold text-xl">Takuya Saito</h2>
-      <h6 className="mt-2 text-sm font-medium">
+      <p className="mt-2 text-sm font-medium">
         System Engineer
-      </h6>
+      </p>
 
       <p className="text-xs text-gray-500 text-center mt-3">
         Next.js,TypeScriptをやっていく
@@ -29,16 +29,25 @@ export const ProfileCard: FC<Props> = (props) => {
       <ul className="flex flex-row mt-4 space-x-2">
         <ProfileIcon
           href="https://github.com/tktk7l9"
+          name="github"
         >
           <GithubIcon />
         </ProfileIcon>
-        {/* <ProfileIcon href="https://zenn.dev/tktk7l9">
+        {/* <ProfileIcon href="https://zenn.dev/tktk7l9"
+        name="zenn"
+        >
           <ZennIcon color="currentColor" />
         </ProfileIcon> */}
-        <ProfileIcon href="https://twitter.com/tktk7l9">
+        <ProfileIcon
+          href="https://twitter.com/tktk7l9"
+          name="twitter"
+        >
           <TwitterIcon />
         </ProfileIcon>
-        <ProfileIcon href="mailto:saitotakuya0719@gmail.com">
+        <ProfileIcon
+          href="mailto:saitotakuya0719@gmail.com"
+          name="email"
+        >
           <EmailIcon />
         </ProfileIcon>
       </ul>
